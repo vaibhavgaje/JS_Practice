e00141f8c411ee9ff3b7438a8b7db283aa8123c3
Vaibhav Gaje <Vaibhavgaje96@gmail.com>
2024-10-05 13:11:48 +0530

    callback_hell

--- JS_NAMASTE_JS_SEASON2/index.js
+++ JS_NAMASTE_JS_SEASON2/index.js
@@ -4,9 +4,24 @@
 //     console.log("Javascript");
 // },5000)
 
+/*
+Two issues while using callbacks
 
+1 - Callback hell
+When a function is passed as an argument to another function, it becomes a callback function. This process continues and there are many callbacks inside another's Callback function.
+This grows the code horizontally instead of vertically. That mechanism is known as callback hell. 
+
+2 - Inversion of control
+The callback function is passed to another callback, this way we lose the control of our code. We don't know what is happening behind the scene and the program becomes very difficult to maintain. 
+That process is called inversion of control. 
+*/
+//1 - Callback hell
 const cart = ["shoes", "pants", "kurta"];
 
-api.createOrder()
-
- 
+api.createOrder(cart, function () {
+  api.proceedToPayment(function () {
+    api.showOrderSummery(function () {
+      api.updateWallet();
+    });
+  });
+});
